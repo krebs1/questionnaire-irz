58891c5f50643ec80a5a58763a8cacc032b8980f
krebs1 <fantom.candle@mail.ru>
2023-12-20 00:44:47 +0400

    feature: add basic repository structure

---
+++ Repository/RepositoryWrapper.cs
@@ -0,0 +1,20 @@
+﻿using Contracts;
+using Entities;
+
+namespace Repository
+{
+    public class RepositoryWrapper : IRepositoryWrapper 
+    { 
+        private RepositoryContext _repoContext; 
+        
+        public RepositoryWrapper(RepositoryContext repositoryContext) 
+        { 
+            _repoContext = repositoryContext; 
+        } 
+        
+        public void Save() 
+        {
+            _repoContext.SaveChanges();
+        } 
+    }
+}
